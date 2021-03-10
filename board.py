edfe5d1e132a57c12cc786c46046d132409db4c0
Kori Vernon <korivernon@nyu.edu>
2021-03-09 19:01:14 -0500

    added function to load board from testfile

---
+++ board.py
@@ -0,0 +1,47 @@
+class Board:
+    def __init__(self, filename = ""):
+        self.pre = []
+        self.post = []
+        if filename != "":
+            self.filename = filename
+            readFromFile(filename, self.pre, self.post)
+        else:
+            self.filename = filename
+    def loadBoard(self, filename):
+        self.filename = filename
+        readFromFile(self.filename, self.pre, self.post)
+
+def readFromFile(filename, pre, post):
+    '''
+    readFromFile is passed a filename in the format
+    that we expect. Everything from the file
+    will be stored in a two dimensional array.
+
+    precondition -> filename
+    postcondition -> 2D data array
+    return: list
+    '''
+    inFile = open(filename, "r")
+    count = 0
+    for line in inFile:
+        count += 1
+        line = line.strip().split(" ")
+        if count < 5:
+            pre.append(line)
+        elif count > 5:
+            post.append(line)
+    inFile.close()
+    return True
+
+def printPretty(param):
+    if param != []:
+        for i in range(len(param)):
+            for j in range(len(param[i])):
+                print(param[i][j] + " ", end="", sep=" ")
+            print()
+
+def main():
+    testBoard = Board("test.txt")
+    printPretty(testBoard.pre)
+    printPretty(testBoard.post)
+main()
