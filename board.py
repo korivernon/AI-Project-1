d1156b06ffab71afb5dee19d6255ce24cf7c5650
Kori Vernon <korivernon@nyu.edu>
2021-04-03 18:39:00 -0400

    hey b

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 from function import *
 import copy
 BLANK = "BLANK"
-#hello :)
+#hey bethany
 class Board:
     def __init__(self, board = []):
         self.pre = board #pre refers to the board -> pre should be board. changing in the future
