5f550374c4b05c81f7e734041ee3b4358d73d1da
Bethany Saunders <bethanysaunders@10-21-117-190.dynapool.wireless.nyu.edu>
2021-04-11 00:26:23 -0400

    a* algorithm again

--- board.py
+++ board.py
@@ -27,7 +27,7 @@ class Node:
     def __eq__(self, other):
         return self.position == other.position
 
-def return_path(current_node, maze):
+def return_path(current_node, board):
     path = []
     no_rows, no_columns =
     result = [[-1 for i in range (no_columns)] for j in range (no_rows)]
@@ -42,7 +42,7 @@ def return_path(current_node, maze):
         start_value += 1
     return result
 
-def search(maze, cost, start, end):
+def search(board, cost, start, end):
     start_node = Node(None, tuple(start))
     start_node.g = start_node.h = start_node.f = 0
     end_node = Node(None, tuple(end))
@@ -51,11 +51,59 @@ def search(maze, cost, start, end):
     visited_list = []
     yet_to_visit_list.append(start_node)
     outer_iterations = 0
-    max_iterations = (len(maze) // 2) ** 10
+    max_iterations = (len(board) // 2) ** 10
 
     #go up, go left, go down, go right
     move = [[-1,0],[0,-1], [1,0], [0,1]]
 
+    no_rows, no_columns = np.shape(board)
+
+    while len(yet_to_visit_list) > 0:
+        outer_iterations += 1
+
+        current_node = yet_to_visit_list[0]
+        current_index = 0
+        for index, item in enumerate(yet_to_visit_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        if outer_iterations > max_iterations:
+            print("Done pathfinding, too many iterations occurred")
+            return return_path(current_node,board)
+
+        yet_to_visit_list.pop(current_index)
+        visited_list.append(current_node)
+
+        if current_node == end_node:
+            return return_path(current_node, board)
+
+        # Generate children from all adjacent squares
+        children = []
+        for new_position in move:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            if (node_position[0] > (no_rows -1) or node_position[0] < 0 or node_position[1] > (no_columns-1) or node_position[1] < 0):
+                continue
+
+            if board[node_position[0]][node_position[1]] != 0:
+                continue
+
+            new_node = Node(current_node, node_position)
+            children.append(new_node)
+
+            for child in children:
+                if len([visited_child for visited_child in visited_list if visited_child == child]) > 0:
+                    continue
+
+                child.g = current_node.g + cost
+                child.h = (((child.position[0] - end_node.posiiton[0]) ** 2) +((child.position[1] - end_node.position[1]) ** 2))
+
+                child.f = child.g + child.h
+
+                if len([i for i in yet_to_visit_list if child == i and child.g > i.g]) > 0:
+                    continue
+
+                yet_to_visit_list.append(child)
 
 
 def readFromFile(filename):
