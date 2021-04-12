96ce6de8936d9c9180837092b583888bddc84eb5
Kori Vernon <korivernon@nyu.edu>
2021-04-11 21:02:47 -0400

    finishing the job

--- init.py
+++ init.py
@@ -1,9 +1,14 @@
 from board import Board, readFromFile, stats, makeMove
 
+INP1 = "inp/Input1.txt"
+INP2 = "inp/Input2.txt"
+INP3 = "inp/Input3.txt"
+
 def main():
     # initialize test board
     # testBoard = Board("test.txt")
-    pre, post = readFromFile("inp/input1.txt")
+
+    pre, post = readFromFile(INP1)
     print(pre, post, sep="\n\n")
 
     stats(pre, post)
