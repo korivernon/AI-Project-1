0e5c3d16fbc8052e846b70ef8396d8b549dbb773
Kori Vernon <korivernon@nyu.edu>
2021-04-12 18:40:21 -0400

    latest

--- init.py
+++ init.py
@@ -8,19 +8,11 @@ SO = "inp/Sample_Output.txt"
 left = 1;left_up = 2;up = 3; right_up = 4; right = 5; right_down = 6; down = 7; left_down = 8
 
 '''
-def output(pre, post, nodes, len):
-    outFile = "{}\n\n{}\n\n{}\n{}".format(pre, post, nodes, len)
-    print(outFile)
-'''
-'''
     Need to use a dictionary so that if flag = True, then we append 
         the dictionary key to a list, then return that list to init.py
     Need a function to compare the current node to the previous 
         constantly in the while t loop
 '''
-def compare():
-    if (True):
-        print("blah")
 
 def main():
     # initialize test board
@@ -32,7 +24,7 @@ def main():
     result, nodes= AStar(pre, post)
     numMoves = 0
 
-    print("goal:\n{}\npre:\n{}".format(post, pre))
+    #print("goal:\n{}\npre:\n{}".format(post, pre))
 
 
     if (not result):
@@ -44,14 +36,8 @@ def main():
         prev = t
         while t:
             numMoves +=1
-            print(t)
-            print()
             t = t.parent
-            # try:
-            #     continue
-            #     print(t.f)
-            # except AttributeError:
-            #     continue
+
     #output(pre, post, numMoves, nodes)
 
     # stats(pre, post)
