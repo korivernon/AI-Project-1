99bc21215a887d17281301210a728768e00c4b5c
Kori Vernon <korivernon@nyu.edu>
2021-04-14 21:36:49 -0400

    adding comments

--- blsksv_project_1.py
+++ blsksv_project_1.py
@@ -51,7 +51,7 @@ class Board:
         for i in range(len(flat)):
             for tile in flat:
                 if (tile.val == i):
-                    currTile = tile
+                    currTile = tile #find the location of the tile
                 if (tile.goal == i):
                     goalTile = tile # we can just use rise over run instead of absolute value x2-x1/y2-y1?
             total += abs(currTile.x - goalTile.x) + abs(currTile.y - goalTile.y) #how to alter for diag..?
@@ -65,118 +65,109 @@ class Board:
             return
         self.swap_placement(self.blank, after)
 
-    def swap_placement(self, before, after):
+    def swap_placement(self, before, after): #we want to swap the values at this point,
+                                            # NOT the entire file, because we want the goal to remain static
         self.pre[before[0]][before[1]].val, self.pre[after[0]][after[1]].val = self.pre[after[0]][after[1]].val, self.pre[before[0]][before[1]].val
 
 class Tile:
-    def __init__(self, x, y, curr, goal = None):
-        self.x = x
-        self.y = y
-        self.val = curr
-        self.goal = goal
+    def __init__(self, x, y, curr, goal = None): #when initializing the starting board, we will have to add the gaol at a later time
+        self.x = x; self.y = y #get the x and y values
+        self.val = curr; self.goal = goal #initialize the value and the goal
 
     def addGoal(self, goal): #if we need to add a goal, we have an option to do so
-        self.goal = goal
-        return True
+        self.goal = goal #add the goal later so we can keep track of both the goal and the value in the same ds
 
-    def __str__(self):
+    def __str__(self): #string representation so in order to see exactly what is happening
         st = "x: {}, y: {}, val: {}, goal: {}\t\t|".format(self.x, self.y, self.val, self.goal)
         return st
 
 def readAndLoadFromFile(filename):
-    pre = []; goal = []
+    pre = []; goal = [] #pre represents the start, and goal represents the goal
     try:
-        inFile = open(filename, "r")
+        inFile = open(filename, "r") #we are first going to try to open the file
     except FileNotFoundError:
-        return False
-    for y, line in enumerate(inFile):
-        elems = []
-        line = line.strip().split()
-        line = parseLine(line)
+        return False #if we are unable to open the file, exit the code
+    for y, line in enumerate(inFile): #using enumerate so we can keep track of the y value, as well as line elements
+        elems = [] #adding line by line
+        line = line.strip().split() #splitting by space and stripping new line character
+        line = parseLine(line) #separate the line into a list of integers
         if y < 4:
-            for x, item in enumerate(line):
-                curr = Tile(x,y, item)
-                elems.append(curr)
-            pre.append(elems)
+            for x, item in enumerate(line): # create the two dimensional array
+                curr = Tile(x,y, item) #add as a tile object where item == value
+                elems.append(curr) #append to the elems == line
+            pre.append(elems) #append the elems to the starting node
         elif y > 4:
-            goal.append(line)
+            goal.append(line) #we can just append this to goal, then go back in the future and add it as a goal
     for y, line in enumerate(pre):
         for x, tile in enumerate(line):
-            tile.addGoal(goal[y][x])
-    inFile.close()
-    pre = Board(pre)
+            tile.addGoal(goal[y][x]) #adding the goal at goal[y][x]
+    inFile.close() #closing file because we are done
+    pre = Board(pre) #creating a board object to return
     return pre
 
-def swap_placement(cond, before, after):
-    cond[before[0]][before[1]], cond[after[0]][after[1]] = cond[after[0]][after[1]], cond[before[0]][before[1]]
-    temp = Board(cond)
-    return temp
-
-def getBlankTup(cond):
-    avail = {}
+def getBlankTup(cond): #this will get the blank tuple for us, storing the coordinates of the blank
     for i in range(len(cond)):
         for j in range(len(cond[i])):
-            if cond[i][j].val == BLANK:
-                if (avail != None):
-                    # check all sides
-                    avail = checkAvail((i, j))
-                return (i, j), avail
+            if cond[i][j].val == BLANK: #we want to see if the tile at this point is None
+                avail = checkAvail((i, j)) #we are checking availability at this point
+                return (i, j), avail #returning a tuple of the return objects
     return (-1, -1), {}
 
 def checkAvail(loc ):
     retLst = []
     retDict = {}
-    up = loc[0] > LOWER
-    down = loc[0] < UPPER
-    left = loc[1] > LOWER
-    right = loc[1] < UPPER
+    up = loc[0] > LOWER #so we can check to see if the upper is clear
+    down = loc[0] < UPPER #so we can check to see if the lower is clear
+    left = loc[1] > LOWER #so we can check to see if the left is clear
+    right = loc[1] < UPPER # so we can check to see if the right is clear
+    #check up
     if up:
         retLst.append(
             (loc[0]-1, loc[1])
         )
-        retDict[3] = retLst[-1]
+        retDict[3] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     #check down
     if down:
         retLst.append(
             (loc[0]+1, loc[1])
         )
-        retDict[7] = retLst[-1]
+        retDict[7] = retLst[-1]  #simply taking the value at -1 index and adding to the dictionary with the directional value
     #check left
     if left:
         retLst.append(
             (loc[0], loc[1]-1)
         )
-        retDict[1] = retLst[-1]
+        retDict[1] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     #check right
     if right:
         retLst.append(
             (loc[0], loc[1]+1)
         )
-        retDict[5] = retLst[-1]
+        retDict[5] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     # check left up
     if up and left:
         retLst.append(
             (loc[0] - 1, loc[1] -1 )
         )
-        retDict[2] = retLst[-1]
+        retDict[2] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     #check right up
     if up and right:
         retLst.append(
             (loc[0] - 1, loc[1] + 1 )
         )
-        retDict[4] = retLst[-1]
+        retDict[4] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     # check left down
     if down and left:
         retLst.append(
             (loc[0] + 1, loc[1] - 1)
         )
-        retDict[8] = retLst[-1]
+        retDict[8] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     #check right down
     if down and right:
         retLst.append(
             (loc[0] + 1, loc[1] + 1)
         )
-        retDict[6] = retLst[-1]
+        retDict[6] = retLst[-1] #simply taking the value at -1 index and adding to the dictionary with the directional value
     return retDict
 
 def search(start):
@@ -202,16 +193,16 @@ def search(start):
     return None, None
 
 def findBestf(board):
-    f_lst = []
-    positions = []
-    curr = board
-    while curr.parent != None:
-        f_lst.append(curr.f)
+    f_lst = [] #have a list to store the f values
+    positions = [] #list to store positions
+    curr = board #set the current to the board
+    while curr.parent != None: #continue until we reach the root
+        f_lst.append(curr.f) #append the current f value
         positions.append(curr.positions)
         curr = curr.parent # go back by one node and traverse
-    f_lst.append(curr.f); f_lst.reverse()
-    positions.reverse()
-    return f_lst, positions
+    f_lst.append(curr.f); f_lst.reverse() #reverse the the positions list so we go forward
+    positions.reverse() #reverse the the positions list so we go forward
+    return f_lst, positions #return the tuple
 
 def parseLine(line):
     '''
@@ -236,11 +227,11 @@ def init_board_with_file_and_run(filename):
     if start == False:
         return False
     goal, numNodes = search(start) # starts the search
-    f_lst, positions = findBestf(goal)
-    st = output(board, board.g, numNodes, positions, f_lst)
+    f_lst, positions = findBestf(goal) #obtain the output from backtrack
+    st = output(board, board.g, numNodes, positions, f_lst) #this will give us our intended output in the form of a string
 
-    newFilename = filename[:filename.find(".")] + "_ASTAR_OUTPUT.txt"
-    print(st, newFilename)
+    newFilename = filename[:filename.find(".")] + "_ASTAR_OUTPUT.txt" #have a set output name for ease of use
+    print(st, newFilename) #print the value of the string to the filename
 
     return True
 
